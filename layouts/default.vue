45288d604f3b61d26206c55ed416a0c519be1add
G-shino <shinogu2929@gmail.com>
2020-08-01 13:56:55 +0900

    add layouts dir

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>hoge</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  $color-bg: rgb(255, 0, 255);
+  h1{
+    background-color: $color-bg;
+  }
+</style>
